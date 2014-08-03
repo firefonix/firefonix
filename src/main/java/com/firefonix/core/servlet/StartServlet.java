aad263e8df30cc1e0dc693da02017e0295ac8cd9
icefonix <icefonix@gmail.com>
2014-08-03 11:38:52 +0800

    Bundle的初步设想

--- src/main/java/com/firefonix/core/servlet/StartServlet.java
+++ src/main/java/com/firefonix/core/servlet/StartServlet.java
@@ -1,7 +1,17 @@
 package com.firefonix.core.servlet;
 
+import com.firefonix.core.bundle.Bundle;
+
+import javax.servlet.http.HttpServlet;
+
 /**
  * Created by IceFonix on 2014/8/1.
  */
-public class StartServlet extends Httpservlet {
+public class StartServlet extends HttpServlet {
+    /**
+     * 初始化整个环境
+     *@param fires String[] 初始化文件位置
+     *@param extras Bundle 附加选项
+     * */
+    protected void initLoader(String []fires,Bundle extras){}
 }
