9a5be07e9841eacc9c68bd8e7788de6238db505f
icefonix <icefonix@gmail.com>
2014-08-03 12:29:14 +0800

    init Servlet

--- src/main/java/com/firefonix/core/servlet/StartServlet.java
+++ src/main/java/com/firefonix/core/servlet/StartServlet.java
@@ -1,8 +1,7 @@
 package com.firefonix.core.servlet;
 
-import com.firefonix.core.bundle.Bundle;
-
 import javax.servlet.http.HttpServlet;
+import java.util.Map;
 
 /**
  * Created by IceFonix on 2014/8/1.
@@ -13,5 +12,7 @@ public class StartServlet extends HttpServlet {
      *@param fires String[] 初始化文件位置
      *@param extras Bundle 附加选项
      * */
-    protected void initLoader(String []fires,Bundle extras){}
+    protected void initLoader(String []fires,Map extras){}
+    public Thread st;
+
 }
